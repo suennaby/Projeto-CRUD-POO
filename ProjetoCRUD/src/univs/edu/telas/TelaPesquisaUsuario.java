437be92c0839f313bb87cf78f63bc1056e065898
suennaby <suennabyl@gmail.com>
2019-11-06 23:50:59 -0200

    Inicio Funcionário

--- ProjetoCRUD/src/univs/edu/telas/TelaPesquisaUsuario.java
+++ ProjetoCRUD/src/univs/edu/telas/TelaPesquisaUsuario.java
@@ -95,6 +95,11 @@ public class TelaPesquisaUsuario extends javax.swing.JFrame {
         });
 
         jButton4.setText("Voltar");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -176,6 +181,10 @@ public class TelaPesquisaUsuario extends javax.swing.JFrame {
           dispose();
        }
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        dispose();
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
